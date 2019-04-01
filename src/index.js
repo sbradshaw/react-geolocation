eb272c45646cd04eae6c75d3b190e36ac46eb82e
Stuart Bradshaw <stuartb@Hyperion.local>
2019-04-01 22:13:57 +0100

    Add Material-UI and project dependencies
    * Add Material-UI to the project
    * Add addtional required project dependencies
    * Add a Login.js component and update the router

--- src/index.js
+++ src/index.js
@@ -3,12 +3,14 @@ import ReactDOM from "react-dom";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 import App from "./pages/App";
+import Login from "./pages/Login";
 
 const Root = () => {
   return (
     <Router>
       <Switch>
         <Route exact path="/" component={App} />
+        <Route exact path="/login" component={Login} />
       </Switch>
     </Router>
   );
--- src/withRoot.js
+++ src/withRoot.js
@@ -1,8 +1,36 @@
 import React from "react";
+import { MuiThemeProvider, createMuiTheme } from "@material-ui/core/styles";
+import grey from "@material-ui/core/colors/grey";
+import pink from "@material-ui/core/colors/pink";
+import CssBaseline from "@material-ui/core/CssBaseline";
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      light: grey[300],
+      main: grey[500],
+      dark: grey[800]
+    },
+    secondary: {
+      light: pink[200],
+      main: pink[500],
+      dark: pink[700]
+    }
+  },
+  typography: {
+    useNextVariants: true
+  }
+});
 
 const withRoot = Component => {
   const WithRoot = props => {
-    return <div>withRoot</div>;
+    // MuiThemeProvider makes a theme available down the React tree
+    return (
+      <MuiThemeProvider theme={theme}>
+        <CssBaseline />
+        <Component {...props} />
+      </MuiThemeProvider>
+    );
   };
 
   return WithRoot;
