e89ab5d0fa5fd6f478e717d48d4dcfdfdc2040ca
Stuart Bradshaw <stuartb@Hyperion.local>
2019-04-18 15:14:10 +0100

    Style the blog component and set pin content state
    * Add styling to the blog component
    * Set the pin content state
    * Add functionality to delete draft pins

--- src/components/Pin/CreatePin.js
+++ src/components/Pin/CreatePin.js
@@ -1,8 +1,104 @@
-import React from "react";
+import React, { useState, useContext } from "react";
 import { withStyles } from "@material-ui/core/styles";
+import TextField from "@material-ui/core/TextField";
+import Typography from "@material-ui/core/Typography";
+import Button from "@material-ui/core/Button";
+import AddAPhotoIcon from "@material-ui/icons/AddAPhotoTwoTone";
+import LandscapeIcon from "@material-ui/icons/LandscapeOutlined";
+import ClearIcon from "@material-ui/icons/Clear";
+import SaveIcon from "@material-ui/icons/SaveTwoTone";
+
+import Context from "../../context";
 
 const CreatePin = ({ classes }) => {
-  return <div>CreatePin</div>;
+  const { dispatch } = useContext(Context);
+  const [title, setTitle] = useState("");
+  const [image, setImage] = useState("");
+  const [content, setContent] = useState("");
+
+  const handleSubmit = event => {
+    event.preventDefault();
+    console.log({ title, image, content });
+  };
+
+  const handleDeleteDraft = event => {
+    setTitle("");
+    setImage("");
+    setContent("");
+    dispatch({ type: "DELETE_DRAFT" });
+  };
+
+  return (
+    <form className={classes.form}>
+      <Typography
+        className={classes.alignCenter}
+        color="secondary"
+        component="h2"
+        variant="h4"
+      >
+        <LandscapeIcon className={classes.iconLarge} /> Pin a location
+      </Typography>
+      <div>
+        <TextField
+          label="Title"
+          name="title"
+          placeholder="Insert a pin title"
+          onChange={event => setTitle(event.target.value)}
+        />
+        <input
+          accept="image/*"
+          className={classes.input}
+          id="image"
+          type="file"
+          onChange={event => setImage(event.target.files[0])}
+        />
+        <label htmlFor="image">
+          <Button
+            style={{ color: image && "#f48fb1" }}
+            className={classes.button}
+            component="span"
+            size="small"
+          >
+            <AddAPhotoIcon />
+          </Button>
+        </label>
+      </div>
+      <div className={classes.contentField}>
+        <TextField
+          name="content"
+          label="Content"
+          multiline
+          rows="6"
+          margin="normal"
+          fullWidth
+          variant="outlined"
+          onChange={event => setContent(event.target.value)}
+        />
+      </div>
+      <div>
+        <Button
+          className={classes.button}
+          variant="contained"
+          color="primary"
+          onClick={handleDeleteDraft}
+        >
+          <ClearIcon className={classes.leftIcon} />
+          Discard
+        </Button>
+        <Button
+          type="submit"
+          className={classes.button}
+          variant="contained"
+          color="secondary"
+          disabled={!title.trim() || !image || !content.trim()}
+          onClick={handleSubmit}
+        >
+          Submit
+          <SaveIcon className={classes.rightIcon} />
+        </Button>
+      </div>
+    </form>
+  );
 };
 
 const styles = theme => ({
--- src/components/Pin/NoContent.js
+++ src/components/Pin/NoContent.js
@@ -1,7 +1,23 @@
 import React from "react";
 import { withStyles } from "@material-ui/core/styles";
+import ExploreIcon from "@material-ui/icons/Explore";
+import Typography from "@material-ui/core/Typography";
 
-const NoContent = ({ classes }) => <div>NoContent</div>;
+const NoContent = ({ classes }) => (
+  <div className={classes.root}>
+    <ExploreIcon className={classes.icon} />
+    <Typography
+      align="center"
+      component="h2"
+      color="textPrimary"
+      gutterBottom
+      noWrap
+      variant="h6"
+    >
+      Click on the map to add a pin.
+    </Typography>
+  </div>
+);
 
 const styles = theme => ({
   root: {
