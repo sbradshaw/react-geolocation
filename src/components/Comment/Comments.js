41f79500ae6dd0e9294082caad83f7a57d7e4d47
Stuart Bradshaw <stuartb@Hyperion.local>
2019-05-07 21:19:55 +0100

    Add a create comment mutation

--- src/components/Comment/Comments.js
+++ src/components/Comment/Comments.js
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { Fragment } from "react";
+import distanceInWordsToNow from "date-fns/distance_in_words_to_now";
 import { withStyles } from "@material-ui/core/styles";
 import List from "@material-ui/core/List";
 import ListItem from "@material-ui/core/ListItem";
@@ -17,13 +18,17 @@ const Comments = ({ comments, classes }) => (
         <ListItemText
           primary={comment.text}
           secondary={
-            <Typography
-              className={classes.inline}
-              component="span"
-              color="textPrimary"
-            >
-              {comment.author.name}
-            </Typography>
+            <Fragment>
+              <Typography
+                className={classes.inline}
+                component="span"
+                color="textPrimary"
+              >
+                {comment.author.name}
+              </Typography>
+              &nbsp;-&nbsp;{distanceInWordsToNow(Number(comment.createdAt))}
+              &nbsp;ago
+            </Fragment>
           }
         />
       </ListItem>
